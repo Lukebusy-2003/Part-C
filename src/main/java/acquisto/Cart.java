6a81b05441ffb27880049ed5fa5068afb4dd8e0c
Francesca <162310527+Franv03@users.noreply.github.com>
2026-03-23 17:53:44 +0100

    Aggiunta carrello con servlet

---
+++ src/main/java/acquisto/Cart.java
@@ -0,0 +1,73 @@
+package acquisto;
+
+import prodotto.ProductBean;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Cart {
+
+	// Variabili
+	private final ArrayList<ProductBean> products;
+	private int count;
+	
+	// Costruttore
+	public Cart() {
+		products = new ArrayList<ProductBean>();
+		count = 0;
+	}
+	
+	// Metodo aggiunta prodotto al carrello
+	public void addProduct(ProductBean product, int q) {
+	    if(product != null && q > 0) {
+			if (products.isEmpty()) {
+		        products.add(product);
+		        product.setQuantity(q);
+		        count += q;
+		    } else {
+		        boolean productExists = false;
+		        for (Iterator<ProductBean> it = products.iterator(); it.hasNext(); ) {
+		            ProductBean prod = it.next();
+		            if (prod.getCode() == product.getCode()) {
+		                prod.setQuantity(prod.getQuantity() + q);
+		                count += q;
+		                productExists = true;
+		                break;
+		            }
+		        }
+		        if (!productExists) {
+		            products.add(product);
+		            product.setQuantity(q);
+		            count += q;
+		        }
+		    }
+	
+	    }
+	}    
+	// Metodo rimozione prodotto dal carrello
+	public void deleteProduct(ProductBean product,int quantity) {
+		if(quantity>0) {
+			for(ProductBean prod : products) {
+				if(prod.getCode() == product.getCode()) {
+					products.remove(prod);
+					count = count - quantity;
+					break;
+				}
+			}
+	 	}
+	}	
+	
+	// Metodi get
+	public int getCount(){
+		return count;
+	}
+	
+	public ArrayList<ProductBean> getProducts() {
+		return products;
+	}
+
+	public void clearCart() {
+		products.clear();
+		count = 0;
+	}
+
+}
