b2278162eb6e94eca9be105567d85d727b963113
Francesca <162310527+Franv03@users.noreply.github.com>
2026-03-28 16:06:10 +0100

    Aggiunta admin servlet per la visualizzazione dei prodotti

---
+++ src/main/java/prodotto/AdminServlet.java
@@ -0,0 +1,43 @@
+package prodotto;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Admin")
+public class AdminServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private ProductDaoDataSource productDAO;
+
+    @Override
+    public void init() throws ServletException {
+    	productDAO = new ProductDaoDataSource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ArrayList<ProductBean> prodotti = new ArrayList<>();
+
+        try {
+            prodotti = productDAO.doRetrieveAll(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        request.setAttribute("prodotti", prodotti);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("admin/admin.jsp");
+        dispatcher.forward(request, response);
+    }
+}
